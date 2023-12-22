0ec94ba56455916f1d38f006439bba52c04a0345
lexosi <iamlexosi@gmail.com>
2023-12-22 12:11:49 +0100

    Delete Menu class and update Menu.java

--- app/src/main/java/s3t3e1/GardenShop/domain/Menu.java
+++ app/src/main/java/s3t3e1/GardenShop/domain/Menu.java
@@ -1,14 +1,10 @@
-package s3t3e1;
+package s3t3e1.GardenShop.domain;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import s3t3e1.GardenShop.application.port.out.GardenShopRepository;
 import s3t3e1.GardenShop.application.utilities.Input_sc;
-import s3t3e1.GardenShop.domain.Decoration;
-import s3t3e1.GardenShop.domain.Flower;
-import s3t3e1.GardenShop.domain.GardenShop;
-import s3t3e1.GardenShop.domain.Tree;
 import s3t3e1.GardenShop.domain.enums.Colours;
 import s3t3e1.GardenShop.domain.enums.ProductType;
 import s3t3e1.GardenShop.infrastructure.adapter.repository.TxtGardenShopRepository;
