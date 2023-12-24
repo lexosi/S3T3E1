20b6ff2a807d52b7c9cea5ed90c91873a1df46c3
lexosi <lexosigamer@gmail.com>
2023-12-24 17:05:52 +0100

    Add imports and fix formatting in MenuManagementTest.java

--- app/src/test/java/s3t3e1/MenuManagementTest.java
+++ app/src/test/java/s3t3e1/MenuManagementTest.java
@@ -1,4 +1,14 @@
 import org.junit.jupiter.api.Test;
+
+import s3t3e1.GardenShop.domain.GardenShop;
+import s3t3e1.GardenShop.domain.Product;
+import s3t3e1.GardenShop.domain.enums.ProductType;
+import s3t3e1.GardenShop.infrastructure.cli.menu.MenuManagement;
+
+import s3t3e1.GardenShop.domain.GardenShop;
+
+import s3t3e1.GardenShop.domain.GardenShop;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
