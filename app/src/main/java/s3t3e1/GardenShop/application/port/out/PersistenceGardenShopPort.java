4fb6f6bb0ab8d6492bb999fde1cdb0bada65f439
lexosi <lexosigamer@gmail.com>
2023-12-27 19:34:35 +0100

    Remove PersistenceFlowerShopPort and PersistenceFlowerShopAdapter

--- app/src/main/java/s3t3e1/GardenShop/application/port/out/PersistenceGardenShopPort.java
+++ app/src/main/java/s3t3e1/GardenShop/application/port/out/PersistenceGardenShopPort.java
@@ -1,5 +1,5 @@
 package s3t3e1.GardenShop.application.port.out;
 
-public interface PersistenceFlowerShopPort {
+public interface PersistenceGardenShopPort {
 
 }
--- app/src/main/java/s3t3e1/GardenShop/infrastructure/adapter/PersistenceFlowerShopAdapter.java
+++
@@ -1,5 +0,0 @@
-package s3t3e1.GardenShop.infrastructure.adapter;
-
-public class PersistenceFlowerShopAdapter implements s3t3e1.GardenShop.application.port.out.PersistenceFlowerShopPort {
-
-}
