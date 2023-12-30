6fdc0486c294585b79355c7485b911e8f2c7ec1f
lexosi <lexosigamer@gmail.com>
2023-12-30 16:24:28 +0100

    Add addProduct method to GardenShopRepository

--- app/src/main/java/s3t3e1/GardenShop/application/port/out/GardenShopRepository.java
+++ app/src/main/java/s3t3e1/GardenShop/application/port/out/GardenShopRepository.java
@@ -3,9 +3,11 @@ package s3t3e1.GardenShop.application.port.out;
 import java.util.List;
 
 import s3t3e1.GardenShop.domain.GardenShop;
+import s3t3e1.GardenShop.domain.Product;
 
 public interface GardenShopRepository {
 	
+    void addProduct(Product product);
     GardenShop save(GardenShop gardenShop);
     GardenShop findByName(List<GardenShop> shops, String name);
     void delete(GardenShop gardenShop);
