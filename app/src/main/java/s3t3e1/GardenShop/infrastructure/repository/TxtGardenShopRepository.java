a1b87ae10acd5ed4dcc49672b80dca2958c7ef99
m-maso <146754649+m-maso@users.noreply.github.com>
2023-12-30 12:22:16 +0100

    create TxtProductRepo

--- app/src/main/java/s3t3e1/GardenShop/infrastructure/repository/TxtGardenShopRepository.java
+++ app/src/main/java/s3t3e1/GardenShop/infrastructure/repository/TxtGardenShopRepository.java
@@ -7,55 +7,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
 
-import s3t3e1.GardenShop.application.port.in.AddProduct;
 import s3t3e1.GardenShop.application.port.out.GardenShopRepository;
-import s3t3e1.GardenShop.application.port.out.ProductRepository;
 import s3t3e1.GardenShop.domain.GardenShop;
-import s3t3e1.GardenShop.domain.Product;
-import s3t3e1.GardenShop.domain.enums.ProductType;
 
-public class TxtGardenShopRepository implements ProductRepository, GardenShopRepository {
+public class TxtGardenShopRepository implements GardenShopRepository {
 
 	private String filePath = "GardenShop.txt";
 	
 	public TxtGardenShopRepository(String filePath) {
 		this.filePath = filePath;
 	}
-
-	/* PRODUCT REPOSITORY (shop stock) */
-	@Override
-	public Product save(Product product) {
-		try (FileWriter writer = new FileWriter(filePath, true)) {
-			writer.write(product.toString() + "\n");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return product;
-	}
-
-	@Override
-	public Product findProdById(GardenShop gardenShop, int prodId) {
-		Product product = gardenShop.findKeys().stream()
-				.filter(p -> p.getId() == prodId)
-				.findFirst()
-				.orElse(null);
-
-		return product;
-	}
-
-	@Override
-	public void delete(GardenShop gardenShop, int prodId, int quantity) {
-		Product prodFound = findProdById(gardenShop, prodId);
-		if (prodFound != null) {
-			gardenShop.getGardenProducts().remove(prodFound, quantity);
-		} else {
-			System.out.println("Sorry, we can't delete this product. It doesn't exist in our shop's stock");
-		}
-	}
-
+	
 	/* GARDEN SHOP REPOSITORY */
 	@Override
 	public GardenShop save(GardenShop gardenShop) {
--- app/src/main/java/s3t3e1/GardenShop/infrastructure/repository/TxtProductRepository.java
+++ app/src/main/java/s3t3e1/GardenShop/infrastructure/repository/TxtProductRepository.java
@@ -1,5 +1,75 @@
 package s3t3e1.GardenShop.infrastructure.repository;
 
-public class TxtProductRepository {
+import java.io.FileWriter;
+import java.io.IOException;
 
+import s3t3e1.GardenShop.application.port.out.ProductRepository;
+import s3t3e1.GardenShop.domain.GardenShop;
+import s3t3e1.GardenShop.domain.Product;
+
+public class TxtProductRepository implements ProductRepository {
+
+	private String filePath = "data/GardenShopProducts.txt";
+	
+	public TxtProductRepository(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	@Override
+	public Product save(Product product) {
+		try (FileWriter writer = new FileWriter(filePath, true)) {
+			writer.write(product.toString() + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return product;
+	}
+
+	@Override
+	public Product findProdById(GardenShop gardenShop, int prodId) {
+		Product product = gardenShop.findKeys().stream()
+				.filter(p -> p.getId() == prodId)
+				.findFirst()
+				.orElse(null);
+
+		return product;
+	}
+
+	@Override
+	public void delete(GardenShop gardenShop, int prodId, int quantity) {
+		
+		
+	}
+
+	/* PRODUCT REPOSITORY (shop stock) 
+	@Override
+	public Product save(Product product) {
+		try (FileWriter writer = new FileWriter(filePath, true)) {
+			writer.write(product.toString() + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return product;
+	}
+
+	@Override
+	public Product findProdById(GardenShop gardenShop, int prodId) {
+		Product product = gardenShop.findKeys().stream()
+				.filter(p -> p.getId() == prodId)
+				.findFirst()
+				.orElse(null);
+
+		return product;
+	}
+
+	@Override
+	public void delete(GardenShop gardenShop, int prodId, int quantity) {
+		Product prodFound = findProdById(gardenShop, prodId);
+		if (prodFound != null) {
+			gardenShop.getGardenProducts().remove(prodFound, quantity);
+		} else {
+			System.out.println("Sorry, we can't delete this product. It doesn't exist in our shop's stock");
+		}
+	}
+*/
 }
